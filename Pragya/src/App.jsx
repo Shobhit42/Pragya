f1f0a7be8e4a814277f3525991756cc9d648a97b
Shobhit Singh <singh.shobhit4225@gmail.com>
2024-03-30 14:09:41 +0530

    deploy

---
+++ Pragya/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import myImage from './assets/photo.jpg';
+
+function App() {
+  return (
+    <div>
+      <img style={{width:200}} src={myImage} alt="image" />
+      <div style={{fontSize:30}}>She is Pragya duskimal Padumal of Nalasopara</div>
+    </div>
+  )
+}
+
+export default App
